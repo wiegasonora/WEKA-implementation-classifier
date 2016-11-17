d29e74bf9734e9baf16fdb14c2190999929b320d
fazarafi <faza.thirafi@gmail.com>
2016-11-17 17:46:36 +0700

    FFNN

--- src/tubes2ai/OurNeuron.java
+++ src/tubes2ai/OurNeuron.java
@@ -5,29 +5,34 @@
  */
 package tubes2ai;
 
+import java.util.Random;
+
 /**
  *
  * @author user
  */
 public class OurNeuron {
-    private double[][] weight;
-    private int numInput;
-    private int numOutput;
-    private int numHiddenNeuron;
-    private static double bias;
+    private double[][] inWeight;
+    private double[][] outWeight;
+    private double[] output;
+    private int numInput; //number of attribute in the Instances
+    private int numOutput; //number of class in the Instances
+    private int numHiddenNeuron = 0;
     private final double learningRate;
+    private boolean isMulti = false;
     
     private double[] dataInput;
     
-    public OurNeuron(double[] inputData, boolean isAnyHidden, int hiddenNeurons, double learningRateInput) {
+    public OurNeuron(double[][] inputData, int numClasses, int numAttributes, int hiddenNeurons, double learningRateInput) {
         
-        numInput = inputData.length;
-        numHiddenNeuron = hiddenNeurons;
-        dataInput = inputData;
+        numInput = numAttributes;
+        numOutput = numClasses;
+        
         learningRate = learningRateInput;
         
-        if (isAnyHidden) {
-            
+        if (hiddenNeurons >0) {
+            isMulti = true;
+            numHiddenNeuron = hiddenNeurons;
         }
         else {
             
@@ -35,7 +40,42 @@ public class OurNeuron {
     }
     
     public void initializeWeight() {
+        if (isMulti) {
+            //
+            inWeight = new double[numInput+1][numHiddenNeuron+1];            
+            for (int i = 1; i <= numInput; i++) {
+                for (int j = 1; j <= numHiddenNeuron; j++) {
+                    inWeight[i][j] = generateWeight();
+                }
+            }
+            
+            outWeight = new double[numOutput+1][numHiddenNeuron+1];            
+            for (int i = 1; i <= numOutput; i++) {
+                for (int j = 1; j <= numHiddenNeuron; j++) {
+                    outWeight[i][j] = generateWeight();
+                }
+            }
+        }
+        else {
+            outWeight = new double[numOutput+1][numInput+1];            
+            for (int i = 1; i <= numOutput; i++) {
+                for (int j = 1; j <= numInput; j++) {
+                    outWeight[i][j] = generateWeight();
+                }
+            }
+        }
+    }
+    
+    public double generateWeight() {
+        double val;
+        double rangeMin = -3.0;
+        double rangeMax = 3.0;
         
+        
+        Random r = new Random();
+        val = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+        
+        return val;
     }
     
     public void updateWeight() {
@@ -48,15 +88,25 @@ public class OurNeuron {
     
     public double calcHiddenError() {
         double total = 0;
-       
+        
         
         return total;
     }
     
-    public double calcOutputError() {
+    public double calcOutputError(int outId) {
         double total = 0;
         
         return total;
     }
     
+    public double calcSigmoid(int nodeId) {
+        double val = 0;
+        if (!isMulti) {
+            
+        }
+        else {
+            
+        }
+        return val;
+    }
 }
