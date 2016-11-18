fb5ad66c32b3d18c3ccf448cc04eeaac982f16f0
Wiega Sonora <wiegasonora@gmail.com>
2016-11-18 07:02:55 +0700

    buildClassifier udh jadi matrixnya

--- src/tubes2ai/FFDDClassifier.java
+++ src/tubes2ai/FFDDClassifier.java
@@ -19,28 +19,93 @@ public class FFDDClassifier implements Classifier {
     private static int numAttributes;
     private static Instances mainInstances;
     
+    private double[][] inWeight;
+    private double[][] outWeight;
+    private double[] output;
+
+    
+    private int numInput; //number of attributes in the Instances
+    private int numOutput; //number of classes in the Instances
+    private int numHiddenNeuron = 0;
+    private double learningRate;
+    private boolean isMulti = false;
+    
+    private double[] dataInput;
+    private double[][] dataTraining;
+    
     @Override
     public void buildClassifier(Instances input) throws Exception {
-        
+        int zz=0;
+        int i ;
+        int idxArrClass;
+        boolean isInClass;
         mainInstances = new Instances(input);
+        double[] arrClass = new double[mainInstances.numClasses()];
+        for (idxArrClass = 0; idxArrClass < arrClass.length; idxArrClass++){
+            arrClass[idxArrClass] = 0;
+        }
         numAttributes = input.numAttributes();
+        int totInst = mainInstances.numInstances();
+        int n = mainInstances.numAttributes();
+        dataTraining = new double[totInst][n];
+        int idxClass = mainInstances.classIndex();
         Enumeration enu = mainInstances.enumerateInstances();
         while (enu.hasMoreElements()) {
             Instance currInstance = (Instance) enu.nextElement();
-            int n = currInstance.numAttributes();
-            double x,y;
-            for (int i = 0; i < n; i++)
+            isInClass= false;
+            for (i = 0; i < n; i++)
             {
-                x = currInstance.value(i);
-                y = x;
+                dataTraining[zz][i] = currInstance.value(i);
             }
-            
+            int zzz = 0;
+            int xxx = 0;
+            while (zzz < arrClass.length || isInClass){
+                if (arrClass[zzz] == dataTraining[zz][i]){
+                    isInClass = true;
+                }
+                if (arrClass[zzz] != 0){
+                    xxx++;
+                }
+                zzz++;
+            }
+            if (!isInClass){
+               arrClass[xxx] = dataTraining[zz][i];
+            }
+            zz++;
         }
+        
+        //ganti jadi id
+        for (i = 0; i < totInst; i++){
+            int idxtemp = 0;
+            while (idxtemp < arrClass.length){
+                if (arrClass[idxtemp] != dataTraining[i][idxClass]){
+                    idxtemp++;
+                }
+            }
+            dataTraining[i][idxClass] = idxtemp;
+        }
+        
+        //geser
+        double[] arrSwapTemp = new double[mainInstances.numInstances()];
+        for (i = 0; i < totInst; i++){
+            arrSwapTemp[i] = dataTraining[i][idxClass];
+        }
+        
+        for (i = 0; i < totInst; i++){
+            for (int j = idxClass; j < numAttributes; j++){
+                dataTraining[i][j] = dataTraining[i][j+1];
+                if (j == numAttributes -1 ){
+                    dataTraining[i][j] = arrSwapTemp[i];
+                }
+            }
+        }
+        
+        
     }
 
     @Override
     public double classifyInstance(Instance instnc) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return 0;
     }
 
     @Override
